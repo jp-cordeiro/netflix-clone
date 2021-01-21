9bd3bf7559e46b82720560eae5798313b6d80a53
JP <jpc.araujo@hotmail.com>
2021-01-21 16:49:43 -0300

    implementing routes for the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import FaqsContainer from './containers/faqsContainer';
-import FooterContainer from './containers/footerContainer';
-import JumbotonContainer from './containers/jumbotronContainer';
+import { Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import Home from './pages/home';
+import Browse from './pages/browse';
+import SignIn from './pages/signin';
+import SignUp from './pages/signup';
 
 function App() {
   return (
-    <div className="App">
-      <JumbotonContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </div>
+    <Switch>
+      <Route exact path={ROUTES.HOME} component={Home} />
+      <Route path={ROUTES.BROWSE} component={Browse} />
+      <Route path={ROUTES.SIGN_IN} component={SignIn} />
+      <Route path={ROUTES.SIGN_UP} component={SignUp} />
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import App from './App';
 import { GlobalStyles } from './global-styles';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <>
+    <BrowserRouter basename="/netflix-clone/">
       <GlobalStyles />
       <App />
-    </>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
